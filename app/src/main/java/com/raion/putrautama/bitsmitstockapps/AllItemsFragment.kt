b6d851cd8a8b81825c91f20824d1373958bad7a8
amira <amirarahmah1@gmail.com>
2018-05-05 03:55:13 +0700

    fetch all items from firebase

--- app/src/main/java/com/raion/putrautama/bitsmitstockapps/AllItemsFragment.kt
+++ app/src/main/java/com/raion/putrautama/bitsmitstockapps/AllItemsFragment.kt
@@ -2,11 +2,23 @@ package com.raion.putrautama.bitsmitstockapps
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.raion.putrautama.bitsmitstockapps.adapter.BarangAdapter
+import com.raion.putrautama.bitsmitstockapps.model.Barang
+import kotlinx.android.synthetic.main.fragment_all_items.*
 
 class AllItemsFragment : Fragment() {
+
+    var listItems: ArrayList<Barang> = arrayListOf()
+    lateinit var mAdapter : BarangAdapter
+    lateinit var nama : String
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -16,6 +28,39 @@ class AllItemsFragment : Fragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+
+        val mRef = FirebaseDatabase.getInstance().reference
+        mRef.child("barang").addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot?) {
+                listItems.clear()
+                for (data in p0!!.children) {
+
+                    for(barang in data.children){
+                        nama = barang.child("nama_barang").value.toString()
+                        val foto = barang.child("foto").value.toString()
+                        val jumlah = barang.child("jumlah").value.toString()
+                        val harga = barang.child("harga").value.toString()
+
+                        listItems.add(Barang(foto, harga, jumlah, nama))
+
+                    }
+
+                }
+
+                mAdapter = BarangAdapter(listItems)
+                recyclerView.adapter = mAdapter
+                mAdapter.notifyDataSetChanged()
+
+            }
+        })
+
 
     }
 }
---
+++ app/src/main/java/com/raion/putrautama/bitsmitstockapps/adapter/BarangAdapter.kt
@@ -0,0 +1,33 @@
+package com.raion.putrautama.bitsmitstockapps.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.raion.putrautama.bitsmitstockapps.R
+import com.raion.putrautama.bitsmitstockapps.model.Barang
+
+class BarangAdapter(val listBarang : ArrayList<Barang>) : RecyclerView.Adapter<BarangAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent!!.context)
+                .inflate(R.layout.list_barang, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listBarang.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder?, position: Int) {
+        holder?.namaBarangTv?.text = listBarang.get(position).nama_barang
+        holder?.hargaBarangTv?.text = listBarang.get(position).harga.toString()
+    }
+
+    class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        val namaBarangTv = itemView?.findViewById<TextView>(R.id.nama_barang_tv)
+        val hargaBarangTv = itemView?.findViewById<TextView>(R.id.harga_barang_tv)
+
+    }
+}
